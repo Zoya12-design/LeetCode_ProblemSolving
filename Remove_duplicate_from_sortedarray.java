892ba5a71aa441c14ed8d582b1e09e693658b721
Zoya Zindagi <zoyazindgi07@gmail.com>
2025-07-07 15:59:10 +0500

    Remove_duplicate_from_sortedarray

---
+++ Remove_duplicate_from_sortedarray.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int k = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[k - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        int k = removeDuplicates(nums);
+
+        System.out.println("k = " + k);
+        System.out.print("Updated array: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
